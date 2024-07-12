a596f0caca855c8617dd574cf2638d9b66e09693
Love4sharma <www.lovesharma2004@gmail.com>
2024-07-12 15:49:10 +0530

    gvgh

---
+++ question 1/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = 8085;
+
+app.get("/products", async (req, res) => {
+  const { top, minPrice, maxPrice } = req.query;
+
+  const apiUrl = `http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=${top}&minPrice=${minPrice}&maxPrice=${maxPrice}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const products = response.data;
+
+    res.render("products", { products });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server is running");
+});
